364d557dbf146249efb49b0570e84fd3fd8944b2
Nkechi Asuai <nasuai3880@gmail.com>
2022-04-11 19:38:57 -0700

    101-quote.c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 
 /**
 * main - entry point
-* Desription: prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+* Desription: prints exactly "and that piece of art is useful"
+* - Dora Korpar, 2015-10-19",
 * Return: Always 0
 */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-} 
+}
