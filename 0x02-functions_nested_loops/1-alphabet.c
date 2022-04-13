f301f39513d06bd4867c8c4b8f224392d9c40196
Nkechi Asuai <nasuai3880@gmail.com>
2022-04-12 19:45:27 -0700

    1-alphabet.c task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - entry point
+* Description: Prints the alphabet in lowercase followed by a new line.
+* Return: 0
+*/
+
+void print_alphabet(void)
+{
+
+char c = 'a';
+
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+
+}
+_putchar('\n');
+
+}
