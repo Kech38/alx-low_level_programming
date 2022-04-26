366b245b96efd35a21b67fa5653661bb1331cd31
Kech38 <nasuai3880@gmail.com>
2022-04-25 18:50:01 -0700

    0-strcat.c task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,30 @@
 #include "main.h"
-#include <string.h>
 /**
- * _strcat - Entry Function
- * @dest: pointer to char type
- * @src: pointer to char type
- * Return: 0
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ *
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
- int i, dest_len, src_len;
- dest_len = strlen(dest);
- src_len = strlen(src);
- for (i = 0; i < src_len + 1; ++i)
- {
- dest[dest_len + i] = src[i];
- }
- return (dest);
+int e;
+int f;
+
+e = 0;
+
+while (dest[e] != 0)
+{
+e++;
+}
+
+f = 0;
+
+while (src[f] != 0)
+{
+dest[e] = src[f];
+e++;
+f++;
+}
+return (dest);
 }
