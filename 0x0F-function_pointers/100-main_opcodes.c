414f7eec2675ae20ca4244044fab8dd4a2d0c660
Kech38 <nasuai3880@gmail.com>
2022-05-13 20:05:11 -0700

    Advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,8 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-int i, bytes;
-char *ar;
+int bytes, i;
+char *arr;
 
 if (argc != 2)
 {
@@ -25,15 +25,15 @@ if (bytes < 0)
 printf("Error\n");
 exit(2);
 }
-ar = (char *)main;
+arr = (char *)main;
 for (i = 0; i < bytes; i++)
 {
 if (i == bytes - 1)
 {
-printf("%02hhx\n", ar[i]);
+printf("%02hhx\n", arr[i]);
 break;
 }
-printf("%02hhx", ar[i]);
+printf("%02hhx", arr[i]);
 }
 return (0);
 }
