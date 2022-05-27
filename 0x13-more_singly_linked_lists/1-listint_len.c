06655470f01e68130617ecb13ed69dba032c3f80
Kech38 <101760876+Kech38@users.noreply.github.com>
2022-05-27 06:11:42 +0100

    made changes to task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,22 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked list
- * @h: linked list of type listint_t to print
- * Return: number of nodes
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
  */
 
 size_t listint_len(const listint_t *h)
 
 {
-size_t num = 0;
-while (h)
+size_t nnodes = 0;
+
+while (h != NULL)
 {
-num++;
-h->next;
+h = h->next;
+nnodes++;
 }
-return (num);
+return (nnodes);
 }
